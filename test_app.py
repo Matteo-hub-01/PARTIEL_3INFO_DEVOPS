d4e1cb39e50290f1e7a0d1363e34e493a41cffd5
matt <matteo.deregnaucourt.etude@gmail.com>
2025-02-26 12:02:41 +0100

    test badge

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import add, multiply, divide, greet
 
 
 class TestAddFunction(unittest.TestCase):
-    
+
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
